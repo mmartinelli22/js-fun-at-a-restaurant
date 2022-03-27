0da88fa624f4d1d4312b0f6251e0063db144d349
Michael Martinelli <turtelboy123@gmail.com>
2022-03-27 13:29:58 -0600

    add chef js code

--- src/Chef.js
+++ src/Chef.js
@@ -1,12 +1,35 @@
 class Chef {
-  constructor() {
-    function chef(name ,) {
-    var chefDescription = {
-      name:name ,
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+ }
+ greetCustomer(name, isMorning = false) {
+   var greeting;
+   if (isMorning) {
+     greeting = "Good morning,";
+   } else {
+     greeting = "Hello,"
+   }
+   return `${greeting} ${name}!`
+ }
+ checkForFood(menuItem) {
+   var isOnMenu = false;
+   var onMenuMessage = `Yes, we're serving ${menuItem.name} today!`;
+   var notOnMenuMessage = `Sorry, we aren't serving ${menuItem.name} today.`
+   if (menuItem.type === 'breakfast') {
+  isOnMenu = this.restaurant.menus.breakfast.includes(menuItem)
+   };
+   if (menuItem.type === 'lunch'){
+   isOnMenu = this.restaurant.menus.lunch.includes(menuItem)
+ };
+   if (menuItem.type === 'dinner') {
+    isOnMenu = this.restaraunt.menus.dinner.includes(menuItem)
+   };
+if(isOnMenu) {
+  return onMenuMessage;
+} else {
+  return notOnMenuMessage;
 }
-  return chef;
-  }
  }
 }
-
 module.exports = Chef;
